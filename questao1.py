1c77b4255d867bf84988e8ba0c8bfd6ae612c866
caiofov <cfoviana@gmail.com>
2021-11-18 21:39:02 -0300

    ultimas alteracoes

--- questao1.py
+++ questao1.py
@@ -1,7 +1,8 @@
 count=0 #variável que irá contar os números
 
-for i in range(0,5000001,2): #irá passar apenas pelos números pares
+for i in range(0,5000001,2): #irá passar apenas pelos números pares -> uma forma de diminuir o custo do algoritmo
+    
     if (i%49 == 0 and i%37 == 0): #se for divisível por 49 e por 37
         count+=1 #incrementa a variavel
 
-print(count)
+print("A quantidade de números entre 1 e 5000000 que seguem os requisitos é", count)
