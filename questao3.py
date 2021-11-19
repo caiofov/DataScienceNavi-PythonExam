7e90b55ccc7f7b465447e86d129beeecd83c2108
caiofov <cfoviana@gmail.com>
2021-11-18 21:28:10 -0300

    comentando questao 3

--- questao3.py
+++ questao3.py
@@ -1,14 +1,15 @@
 
-notas = {}
+notas = {} #dicionario de notas
 
-for i in range(5):
-    aluno = "aluno "+str(i+1)
+for i in range(5): #loop roda 5 vezes
+    aluno = "aluno "+str(i+1) #o aluno a qual o loop está referenciando
     
-    nota = float(input("Insira a nota do %s: " %aluno))
+    nota = float(input("Insira a nota do %s: " %aluno)) #recebe o input
     
-    notas[aluno] = nota
+    notas[aluno] = nota #guarda a nota no dicionario
 
-alunoMaior = max(notas, key=notas.get)
-print("A maior nota é do %s e foi %.2f" %(alunoMaior, notas[alunoMaior]))
+maxAluno = max(notas, key=notas.get) #acha a chave do dicionario que possui o maior valor
+
+print("A maior nota é do %s e foi %.2f" %(maxAluno, notas[maxAluno]))
 
 
