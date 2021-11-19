b8b6d20c83851f30d08466b06dedaa8e4de8f515
caiofov <cfoviana@gmail.com>
2021-11-18 21:32:12 -0300

    questao 2

--- questao2.py
+++ questao2.py
@@ -3,12 +3,11 @@ from math import factorial, log
 vetor = []
 
 for i in range(10): #vetor de 10 posiçoes (0 a 9)
-    if(i%2==0): #se for par
+    if(i%2 == 0): #se for par
         vetor.append(pow(3,i)+7*factorial(i)) 
 
     else: #se for impar
         vetor.append(2+4*log(i))
 
 
-for el in vetor:
-    print(el)
+print("Maior elemento: %.2f \nMédia: %.2f" %(max(vetor), (sum(vetor)/len(vetor)) ))
