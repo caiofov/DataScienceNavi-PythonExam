cf7c1d1f9456669592ff9325310bbc6e15f413f4
caiofov <cfoviana@gmail.com>
2021-11-18 21:05:55 -0300

    questao 2

---
+++ questao2.py
@@ -0,0 +1,14 @@
+from math import factorial, log
+
+vetor = []
+
+for i in range(10): #vetor de 10 posiçoes (0 a 9)
+    if(i%2==0): #se for par
+        vetor.append(pow(3,i)+7*factorial(i)) 
+
+    else: #se for impar
+        vetor.append(2+4*log(i))
+
+
+for el in vetor:
+    print(el)
